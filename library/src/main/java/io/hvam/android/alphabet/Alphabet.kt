1cb34d57d7d6dd600281d1955b475ac339df30b8
Danny Hvam <dkhvam@gmail.com>
2020-04-04 22:38:56 +0200

    Add interface documentation and alphabet chain test

--- library/src/main/java/io/hvam/android/alphabet/Alphabet.kt
+++ library/src/main/java/io/hvam/android/alphabet/Alphabet.kt
@@ -1,6 +1,18 @@
 package io.hvam.android.alphabet
 
+/**
+ * Alphabet is an interface for handling encoding and decoding of various UTF-8 characters, and
+ * strings. The nature of each alphabet in implementation specific.
+ */
 interface Alphabet {
+
+    /**
+     * Encode a string (usually a character) from one alphabet to another.
+     */
     fun encode(character: String): String
+
+    /**
+     * Decode a string (usually a character) from one alphabet to another.
+     */
     fun decode(character: String): String
 }
---
+++ library/src/test/java/io/hvam/android/alphabet/AlphabetChainTest.kt
@@ -0,0 +1,21 @@
+package io.hvam.android.alphabet
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AlphabetChainTest {
+
+    @Test
+    fun chain() {
+        val expected = "alfa"
+        val phoneticDecode = PhoneticNatoAlphabet.danish().decode(expected)
+        assertEquals("a", phoneticDecode)
+        val morseEncode = MorseAlphabet().encode(phoneticDecode)
+        assertEquals("·−", morseEncode)
+        val morseDecode = MorseAlphabet().decode(morseEncode)
+        val phoneticEncode = PhoneticNatoAlphabet.danish().encode(morseDecode)
+        assertEquals(expected, phoneticEncode)
+    }
+
+}
