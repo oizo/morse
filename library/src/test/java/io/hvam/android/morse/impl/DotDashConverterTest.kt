2c4aa954bdaf35df4765c1a40ad2c272afb54f29
Danny Hvam <dkhvam@gmail.com>
2020-09-05 23:50:31 +0200

    Add unit tests

---
+++ library/src/test/java/io/hvam/android/morse/impl/DotDashConverterTest.kt
@@ -0,0 +1,35 @@
+package io.hvam.android.morse.impl
+
+import io.hvam.android.morse.MorseSymbol
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class DotDashConverterTest {
+
+    private val sut = DotDashConverter()
+
+    @Test(expected = IllegalAccessException::class)
+    fun testEncode() {
+        sut.encode("this won't work")
+    }
+
+    @Test
+    fun testDecode() {
+        assertEquals("·", sut.decode(MorseSymbol.DOT))
+        assertEquals("−", sut.decode(MorseSymbol.DASH))
+        assertEquals("", sut.decode(MorseSymbol.CHAR_GAP))
+        assertEquals(" ", sut.decode(MorseSymbol.LETTER))
+        assertEquals(" / ", sut.decode(MorseSymbol.WORD))
+    }
+
+    @Test
+    fun testDecodeList() {
+        val tmp = listOf(MorseSymbol.DOT,
+                MorseSymbol.DASH,
+                MorseSymbol.CHAR_GAP,
+                MorseSymbol.LETTER,
+                MorseSymbol.WORD)
+        assertEquals("·−  / ", sut.decode(tmp))
+    }
+
+}
